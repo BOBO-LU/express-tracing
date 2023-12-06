f1d74257f8d44a0dec70e895ef1a8dcab98b41bc
BOBO-LU <luwenkai0820@gmail.com>
2023-12-06 14:54:04 +0800

    add readmd

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Express Request ID Tracing
+
+## install
+```
+npm install
+```
+
+## start
+```
+node app.js
+```
+
+## how to use 
+Send get request to http://localhost:3000 and http://localhost:3000/methodA.
+
+## result
+A file named logs.log will saved the logs and each line has it's request id.
--- service.js
+++ service.js
@@ -2,22 +2,36 @@ const asyncLocalStorage = require("./asyncLocalStorage");
 const { initializeLogger } = require('./logger');
 
 // Initialize the logger
-const logger = initializeLogger(); // Initialize the logger here
+const logger = initializeLogger();
 
 async function methodA() {
+    // this is a time-consuming method, it will call methodB
     const name = 'methodA';
     const requestId = asyncLocalStorage.getStore();
-    logger.info('Start ' + name + ' ' + requestId);
-    await sleep(1000);
-    await methodB();
-    logger.info('Finish ' + name);
+    logger.info(`Start ${name} ${requestId}`);
+
+    try {
+        await sleep(1000);
+        await methodB();
+    } catch (error) {
+        logger.error(`Error in ${name}: ${error.message}`);
+    }
+
+    logger.info(`Finish ${name}`);
 }
 
 async function methodB() {
+    // this is a time-consuming method
     const name = 'methodB';
-    logger.info('Start ' + name);
-    await sleep(500);
-    logger.info('Finish ' + name);
+    logger.info(`Start ${name}`);
+
+    try {
+        await sleep(500);
+    } catch (error) {
+        logger.error(`Error in ${name}: ${error.message}`);
+    }
+
+    logger.info(`Finish ${name}`);
 }
 
 function sleep(ms) {
@@ -26,4 +40,4 @@ function sleep(ms) {
     });
 }
 
-module.exports = {methodA};
+module.exports = { methodA };
